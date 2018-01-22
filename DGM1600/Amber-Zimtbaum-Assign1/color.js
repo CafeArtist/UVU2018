ff5f12fd60b756d0a22293095fc4ba6451caa94f
Amber <10796893@my.uvu.edu>
2018-01-22 08:43:07 -0700

    Ver. 1 of Assign 1

---
+++ DGM1600/Amber-Zimtbaum-Assign1/color.js
@@ -0,0 +1,11 @@
+/*jslint devel: true, node: true*/
+
+// This is a comment, ctrl and / will comment in or out lines
+
+var firstName = prompt('What is your First Name?');
+
+var lastName = prompt('What is your Last Name?');
+
+var favColor = prompt('What is your Favorite Color?');
+
+alert(firstName + ' ' + lastName + ' ' + favColor);
